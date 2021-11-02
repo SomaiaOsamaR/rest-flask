1b77e8d14903eaa5cdc6af6c201b13a611bf41fd
SomaiaOsamaR <93559325+SomaiaOsamaR@users.noreply.github.com>
2021-11-02 06:52:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -182,8 +182,8 @@ api.add_resource(Create_Update,'/')
 
 
 # main function for calling the app
-app.run(debug = True,use_reloader = False)
-dubug = True
+#app.run(debug = True,use_reloader = False)
+#dubug = True
 if __name__ == '__main__':
      app.run(debug=True)
 
